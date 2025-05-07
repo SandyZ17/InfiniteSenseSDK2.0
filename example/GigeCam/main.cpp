dc7ffef7b23556e49d33638b7b4ab80aca56a825
liuyangyang <www.qq837374741@foxmail.com>
2025-05-08 00:01:08 +0800

    format code

--- example/GigeCam/main.cpp
+++ example/GigeCam/main.cpp
@@ -1,10 +1,17 @@
 #include "infinite_sense.h"
 #include "mv_cam.h"
 using namespace infinite_sense;
+void ImuCallback(const void *msg,size_t) {
+
+}
+void ImageCallback(const void *msg,size_t) {
+
+}
 int main() {
   // 1.创建同步器
   Synchronizer synchronizer;
   synchronizer.SetUsbLink("/dev/ttyACM0", 460800);
+
   // 2.配置同步接口
   auto mv_cam = std::make_shared<MvCam>();
   mv_cam->SetParams({{"camera_1", CAM_1}});
@@ -14,20 +21,9 @@ int main() {
   synchronizer.Start();
 
   // 4.接收数据
-  zmq::context_t context(1);
-  zmq::socket_t subscriber(context, zmq::socket_type::sub);
-  subscriber.connect("tcp://localhost:5555");
-
-  const std::string topic = "imu_1";   // 订阅特定主题（如,板载IMU数据： "imu_1"）
-  subscriber.setsockopt(ZMQ_SUBSCRIBE, topic.c_str(), topic.size());
-  zmq::message_t msg;
   while (true) {
-    if (subscriber.recv(msg, zmq::recv_flags::dontwait)) {
-      if (subscriber.get(zmq::sockopt::rcvmore)) {
-        zmq::message_t dummy;
-        subscriber.recv(dummy);
-      }
-    }
+    Messenger::GetInstance().SubStruct("imu_1", ImuCallback);
+    Messenger::GetInstance().SubStruct("camera_1", ImageCallback);
   }
   // 5.停止同步
   synchronizer.Stop();
