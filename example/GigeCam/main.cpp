7f24969e62a401b4a9f4f9a5a47d30420d231cbd
liuyangyang <www.qq837374741@foxmail.com>
2025-05-10 23:56:39 +0800

    update main

--- example/GigeCam/main.cpp
+++ example/GigeCam/main.cpp
@@ -1,10 +1,14 @@
+// 加入SDK头文件
 #include "infinite_sense.h"
+// 加入工业相机头文件
 #include "mv_cam.h"
 using namespace infinite_sense;
+// 自定义回调函数
 void ImuCallback(const void* msg, size_t) {
   const auto* imu_data = static_cast<const ImuData*>(msg);
   // 处理IMU数据
 }
+// 自定义回调函数
 void ImageCallback(const void* msg, size_t) {
   const auto* cam_data = static_cast<const CamData*>(msg);
   // 处理图像数据
@@ -25,12 +29,11 @@ int main() {
   // 4.接收数据
   Messenger::GetInstance().SubStruct("imu_1", ImuCallback);
   Messenger::GetInstance().SubStruct("camera_1", ImageCallback);
-
-  // 5.停止同步
+  // 阻塞线程
   while (true) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
-
+  // 5.停止同步
   synchronizer.Stop();
   return 0;
 }
